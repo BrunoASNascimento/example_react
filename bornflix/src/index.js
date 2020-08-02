3fd7cb04e911977b6b56e34af331a8315bea0cb3
Bruno Nascimento <bruno.nascimento@pluvion.com.br>
2020-08-02 16:57:25 -0300

    Add eul

--- bornflix/src/index.js
+++ bornflix/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import cadastroVideo from './pages/cadastro/video'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
+
+const Page404 = () => (<div>Página</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={cadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Page404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
